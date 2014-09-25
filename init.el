47c68202a9feb338c8ad5b132fc370de813f622d
Eric Christopherson <echristopherson@gmail.com>
2014-09-25 12:51:38 -0500

    use Consolas

--- init.el
+++ init.el
@@ -379,7 +379,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "monotype" :family "Menlo")))))
+ '(default ((t (:inherit nil :stipple nil :background "#1C1C1C" :foreground "#F6F3E8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "outline" :family "Consolas")))))
 
 ;;;;;;;;;;;;;;;;
 ;; Appearance ;;
