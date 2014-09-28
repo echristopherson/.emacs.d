abbfba5ca423152b26847036f59c85bf44ad7976
Eric Christopherson <echristopherson@gmail.com>
2014-09-28 13:00:03 -0500

    adjust indentation

--- init.el
+++ init.el
@@ -609,15 +609,14 @@ point."
 ;; Backup directory instead of *~ files
 ;; From <http://emacswiki.org/emacs/BackupDirectory> and <http://snarfed.org/gnu_emacs_backup_files>
 (make-directory "~/.emacs.d/backup" t)
-(setq
- backup-by-copying t              ; don't clobber symlinks
- backup-directory-alist
- '(("." . "~/.emacs.d/backup"))    ; don't litter my fs tree
- delete-old-versions t
- kept-new-versions 6
- kept-old-versions 2
- version-control t                ; use versioned backups
- )
+(setq backup-by-copying t               ; don't clobber symlinks
+      backup-directory-alist
+      '(("." . "~/.emacs.d/backup"))    ; don't litter my fs tree
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t                 ; use versioned backups
+      )
 
 ;; Put auto-save files (#foo#) in one place
 (setq auto-save-file-name-transforms
